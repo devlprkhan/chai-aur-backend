7ee1e99389ba513f17906d9d01ed6ff0aa77aa5d
Haseeb Khan <81795306+devlprkhan@users.noreply.github.com>
2024-01-24 20:03:55 +0500

    making user and video models

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: './.env'
 })
+const PORT = process.env.PORT || 3000
 
 connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server listening on * http://localhost:${PORT}`);
+    })
+  })
+  .catch((err) => { console.error("MONGO DB Connection Error: ", err) })
